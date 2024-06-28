96532b85c6289f402a835db8a3f5a75088fa9894
Will Holdsworth <will.holdsworth@monash.edu>
2024-06-28 16:48:50 +1000

    change name

---
+++ convert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p temp
+
+# only iterate over the files marked as unfinished
+for file in $(comm -2 -3 all finished); do
+
+	# convert the file
+	./converter $file
+
+	# concatenate the agent results
+	cat $(fd . temp/ | sort -n -t/ -k2) > "$file".out
+
+	# clear agent files for next iteration
+	rm temp/*
+	
+	# mark file as finished
+	echo $file >> finished
+
+done
