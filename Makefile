05267ffc27d4e6d6169c831876279c3eeb53f627
Will Holdsworth <will.holdsworth@monash.edu>
2024-07-01 12:30:04 +1000

    now we need to extract the header and properly structure the rle files

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror -Wpedantic -std=c99
 
-all: converter encoder
+all: converter encoder formatter
 
 converter: converter.c
 encoder: encoder.c
+formatter: formatter.c
 
 clean:
 	rm -f converter encoder
---
+++ formatter.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FILE_MAX 2 << 8 // ext4 file path max size
+#define HEADER_MAX 1000
+
+int main(int argc, char **argv) {
+
+    if (argc < 3) {
+        fprintf(stderr, "expect ./formatter {CSV_PATH} {RLE_PATH}");
+        return EXIT_FAILURE;
+    }
+
+    // get input file names
+    char *in_csv_path = argv[1];
+    char *in_rle_path = argv[2];
+    
+    // open csv file as i1, rle as i2, and new file as o
+
+    FILE *in_csv = fopen(in_csv_path, "r"), *in_rle = fopen(in_rle_path, "r");
+    
+    char out_file_path[FILE_MAX];
+    strcat(out_file_path, in_csv_path);
+    strcat(out_file_path, ".fmt");
+    FILE *out = fopen(out_file_path, "w");
+
+    // write header line of i1 to o
+
+    char header[HEADER_MAX];
+    fscanf(in_csv, "%s\n", header);
+    fprintf(out, header);
+
+    // read first 5 tokens of i1 and write to o
+
+    // read first line of i2 and append to o
+
+    // read all lines of i2 and append to o
+
+    return EXIT_SUCCESS;
+}
