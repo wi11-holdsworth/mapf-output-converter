7b5a694e2bae78df7c241c4587ac271c4067550a
Will Holdsworth <will.holdsworth@monash.edu>
2024-07-01 14:20:38 +1000

    take headers from csv and add to main

--- formatter.c
+++ formatter.c
@@ -4,6 +4,9 @@
 
 #define FILE_MAX 2 << 8 // ext4 file path max size
 #define HEADER_MAX 1000
+#define RECORD_MAX 260000000 // wc -c over all csvs
+#define TOKS_BEFORE_LOG 5
+#define LINE_MAX 290000
 
 int main(int argc, char **argv) {
 
@@ -13,29 +16,49 @@ int main(int argc, char **argv) {
     }
 
     // get input file names
-    char *in_csv_path = argv[1];
-    char *in_rle_path = argv[2];
+    char *in_csv_path = argv[1], *in_rle_path = argv[2];
     
     // open csv file as i1, rle as i2, and new file as o
 
-    FILE *in_csv = fopen(in_csv_path, "r"), *in_rle = fopen(in_rle_path, "r");
+    FILE *in_csv = fopen(in_csv_path, "r");
     
     char out_file_path[FILE_MAX];
-    strcat(out_file_path, in_csv_path);
+    strcpy(out_file_path, in_csv_path);
     strcat(out_file_path, ".fmt");
     FILE *out = fopen(out_file_path, "w");
 
     // write header line of i1 to o
 
     char header[HEADER_MAX];
-    fscanf(in_csv, "%s\n", header);
+    fgets(header, HEADER_MAX, in_csv);
     fprintf(out, header);
 
     // read first 5 tokens of i1 and write to o
 
+    char *record = (char *) malloc(sizeof(char) * RECORD_MAX + 1);
+    fgets(record, RECORD_MAX, in_csv);
+    fprintf(out, "%s,", strtok(record, ","));
+
+    for (int i = 0; i < TOKS_BEFORE_LOG; i++) {
+        fprintf(out, "%s,", strtok(NULL, ","));
+    }
+
+    fclose(in_csv);
+
     // read first line of i2 and append to o
 
+    FILE *in_rle = fopen(in_rle_path, "r");
+
+    char line[LINE_MAX];
+
+    while ((fgets(line, LINE_MAX, in_rle) != NULL)) {
+        fprintf(out, "%s", line);
+    }
+
     // read all lines of i2 and append to o
+
+    fclose(in_rle);
+    fclose(out);
 
     return EXIT_SUCCESS;
 }
